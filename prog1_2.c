cfdebea21aba423f28fe0152278d192968205a5c
Vladimir Blinov <vovka_squid@mail.ru>
2015-07-07 03:19:21 +0300

    prog1_2.c: something new
    Signed-off-by: Vladimir Blinov <vovka_squid@mail.ru>

--- prog1_2.c
+++ prog1_2.c
@@ -3,13 +3,13 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
+#define size 1024
 int main() {
 	int pid;
 	int fd[2];
 	
 	printf("I`m a parrent %d\n", getpid());
 	pipe(fd);
-	//write(fd[1],"data\n", 10);
 	switch(pid = fork()) {
 		case -1:
 			printf("Error");
@@ -17,19 +17,17 @@ int main() {
 		case 0:
 			printf("I`m child and my pid = %d\n", getpid());
 			close(fd[0]);
-			char sym[1024];
 			int ret;
 			dup2(fd[1], 1);
 			ret = execl("/bin/ls", "ls", "/tmp/", "-l", NULL);
 			if (ret == -1)
 				perror("execvp");
 			exit(EXIT_SUCCESS);
-			//printf("sym = %s \n", sym);
-			//return 0;
 	}
 	close(fd[1]);
-	char sym[1024];
-	read(fd[0], sym, 1024);
-	printf("%s \n", sym);
-	//printf("!!!!!!!!!!!!!!22222222222222233333333333333333\n");
+	char sym[size];
+	//int nread;
+	//while ((nread = read(fd[0], sym, size)) > 0)
+	read(fd[0], sym, size);
+	printf("%s\n", sym);
 }
