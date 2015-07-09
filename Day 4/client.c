d10bffea8ea63246da0f69b9e4a589d2e43a950c
Vladimir Blinov <zemetrans@gmail.com>
2015-07-09 18:01:36 +0300

    socket on FS
    Signed-off-by: Vladimir Blinov <zemetrans@gmail.com>

--- Day 4/client.c
+++ Day 4/client.c
@@ -3,23 +3,26 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/un.h>
 
 int main()
 {
 	char message[] = "Hello World!\n";
 	char buf[sizeof(message)];
 	int sock;
-	struct sockaddr_in addr;
-	
-	sock = socket(AF_INET, SOCK_STREAM, 0);
+	//struct sockaddr_un addr;
+	struct sockaddr_un addr;
+	sock = socket(AF_UNIX, SOCK_STREAM, 0);
 	if (sock < 0) {
 		perror("socket");
 		return 1;
 	}
 	
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(3425);
-	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	addr.sun_family = AF_UNIX;
+	
+	strcpy(addr.sun_path, "server_socket");
+	//addr.sin_port = htons(3425);
+	//addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 	if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
 		perror("connect");
 		return 1;
--- Day 4/server.c
+++ Day 4/server.c
@@ -3,23 +3,26 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/un.h>
 
 int main()
 {
 	int sock, listener;
-	struct sockaddr_in addr;
+	struct sockaddr_un addr;
 	char buf[1024];
 	int bytes_read;
 	
-	listener = socket(AF_INET, SOCK_STREAM, 0);
+	listener = socket(AF_UNIX, SOCK_STREAM, 0);
 	if (listener < 0) {
 		perror("socket");
 		return 1;
 	}
 	
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(3425);
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sun_family = AF_UNIX;
+	//addr.sin_port = htons(3425);
+	//addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	unlink("server_socket");
+	strcpy(addr.sun_path, "server_socket");
 	if (bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
 		perror("bind");
 		return 1;
