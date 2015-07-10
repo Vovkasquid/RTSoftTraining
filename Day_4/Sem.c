9df9dfb45275b032ffb38a0ede1bfc7bca3e8b06
Vladimir Blinov <zemetrans@gmail.com>
2015-07-10 14:51:50 +0300

    lectinit
    Signed-off-by: Vladimir Blinov <zemetrans@gmail.com>

--- Day_4/Sem.c
+++ Day_4/Sem.c
@@ -7,14 +7,23 @@ int main() {
 	int nread;
 	int nwrite;
 	int fdin,fdout;
-	int shmid;
+	int shmid, shmid1, shmid2;
 	const int shared_segment_size = 1048576;
 	char* shared_memory;
 	key_t key = ftok("prog1.c", 1);
 	key_t keySem = ftok("prog1.1.c", 1);
+	key_t keySem1 = ftok("file", 1);
+	key_t keySem2 = ftok("file1", 1);
 	int semid = binary_semaphore_allocation(keySem, 0666|IPC_CREAT);
+	int semid1 = binary_semaphore_allocation(keySem1, 0666|IPC_CREAT);
+	int semid2 = binary_semaphore_allocation(keySem2, 0666|IPC_CREAT);
 	
 	binary_semaphore_initialize(semid);
+	binary_semaphore_initialize(semid1);
+	binary_semaphore_initialize(semid2);
+	
+	binary_semaphore_take(semid2);
+	
 	shmid = shmget(key, shared_segment_size, 0666| IPC_CREAT);
 	if (shmid < 0)
 		printf("Shared memory get fail\n");
@@ -38,6 +47,6 @@ int main() {
 		printf("shmdt fail\n");
 	if (shmctl(shmid, IPC_RMID, 0) == -1)
 		printf("shmctl fail\n");
-	binary_semaphore_free(semid);
+	binary_semaphore_deallocate(semid);
 	return 0;
 }
