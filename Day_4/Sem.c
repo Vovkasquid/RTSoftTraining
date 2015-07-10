5feb7a83c1662db47eda216ec8d428ede417a625
Vladimir Blinov <vovka_squid@mail.ru>
2015-07-10 10:59:14 +0300

    Read/write file test
    Signed-off-by: Vladimir Blinov <vovka_squid@mail.ru>

--- Day_4/Sem.c
+++ Day_4/Sem.c
@@ -1,1 +1,20 @@
-//init
+#include "bin_sem.h"
+
+#define BUF_SIZE 1048576
+int main() {
+	char buf[BUF_SIZE];
+	int nread;
+	int nwrite;
+	int fdin;
+	int fdout;
+	
+	fdin = open("100mb_file", O_RDONLY);
+	fdout = open("100mb_file1", O_RDWR|O_CREAT,
+			S_IRUSR|S_IWUSR);
+	while ((nread = read(fdin, buf, BUF_SIZE)) > 0) {
+		//if (nread < BUF_SIZE)
+			//buf[nread] = '\0'
+		nwrite = write(fdout, buf, nread);
+	}
+	return 0;
+}
--- Day_4/bin_sem.h
+++ Day_4/bin_sem.h
@@ -1,6 +1,14 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/shm.h>
 
 union semun { /* Определяем union semun */
 	int val;
