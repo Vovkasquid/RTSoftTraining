959b5635a8ff45590370e3bad9589c15474fa5e8
Vladimir Blinov <vovka_squid@mail.ru>
2015-07-10 11:52:35 +0300

    Home_Work: Create shared memory but don`t have semaphore yet
    Signed-off-by: Vladimir Blinov <vovka_squid@mail.ru>

--- Day_4/Sem.c
+++ Day_4/Sem.c
@@ -1,20 +1,30 @@
 #include "bin_sem.h"
 
 #define BUF_SIZE 1048576
+
 int main() {
 	char buf[BUF_SIZE];
 	int nread;
 	int nwrite;
 	int fdin;
-	int fdout;
+	//int fdout;
+	int shmid;
+	const int shared_segment_size = 1048576;
+	char* shared_memory;
+	key_t key = ftok("prog1.c", 1);
+	shmid = shmget(key, shared_segment_size, 0666| IPC_CREAT);
+	if (shmid < 0)
+		printf("Shared memory get fail\n");
+	shared_memory = shmat(shmid, 0, 0);	
 	
-	fdin = open("100mb_file", O_RDONLY);
-	fdout = open("100mb_file1", O_RDWR|O_CREAT,
-			S_IRUSR|S_IWUSR);
+	fdin = open("bin_sem.h", O_RDONLY);
+	//fdout = open("file1", O_RDWR|O_CREAT,
+			//S_IRUSR|S_IWUSR);
 	while ((nread = read(fdin, buf, BUF_SIZE)) > 0) {
-		//if (nread < BUF_SIZE)
-			//buf[nread] = '\0'
-		nwrite = write(fdout, buf, nread);
+		if (nread < BUF_SIZE)
+			buf[nread] = '\0';
+		//nwrite = write(fdout, buf, nread);
+	strcpy(shared_memory, buf);
 	}
 	return 0;
 }
--- Day_4/Sem1.c
+++ Day_4/Sem1.c
@@ -1,1 +1,29 @@
-//init
+#include "bin_sem.h"
+
+#define BUF_SIZE 1048576
+
+int main() {
+	char buf[BUF_SIZE];
+	int nread;
+	int nwrite;
+	//int fdin;
+	int fdout;
+	int shmid;
+	const int shared_segment_size = 1048576;
+	char* shared_memory;
+	key_t key = ftok("prog1.c", 1);
+	shmid = shmget(key, shared_segment_size, 0666| IPC_CREAT);
+	if (shmid < 0)
+		printf("Shared memory get fail\n");
+	shared_memory = shmat(shmid, 0, 0);	
+	
+	//fdin = open("bin_sem.h", O_RDONLY);
+	fdout = open("file1", O_RDWR|O_CREAT,
+			S_IRUSR|S_IWUSR);
+	sleep(1);
+	do { 
+		nwrite = write(fdout, shared_memory, ((int)strlen(shared_memory)));
+	
+	} while (nwrite == BUF_SIZE);
+	return 0;
+}
