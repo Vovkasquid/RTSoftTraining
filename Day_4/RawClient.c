1a61eac045a213e769ed5a8f54743f53a7093d37
Vladimir Blinov <vovka_squid@mail.ru>
2015-07-11 17:16:26 +0300

    example DGRAM
    Signed-off-by: Vladimir Blinov <vovka_squid@mail.ru>

--- Day_4/RawClient.c
+++ Day_4/RawClient.c
@@ -1,1 +1,25 @@
-init
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+int main() {
+	char msg1[] = "MESSAGE1";
+	char msg2[] = "MESSAGE2";
+	int sock;
+	struct sockaddr_in addr;
+	
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock < 0) {
+		perror("socket");
+		return 1;
+	}
+	
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(3425);
+	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	sendto(sock, msg1, sizeof(msg1), 0, (struct sockaddr *)&addr, sizeof(addr));
+	
+	close(sock);
+	return 0;
+}
--- Day_4/RawServ.c
+++ Day_4/RawServ.c
@@ -1,1 +1,34 @@
-init
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+int main()
+{
+	int sock;
+	struct sockaddr_in addr;
+	char buf[1024];
+	int bytes_read;
+	
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock < 0) {
+		perror("socket");
+		return 1;
+	}
+	
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(3425);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("bind");
+		return 1;
+	}
+	
+	while(1) {
+		bytes_read = recvfrom(sock, buf, 1024, 0, NULL, NULL);
+		buf[bytes_read] = '\0';
+		printf("%s\n", buf);
+	}
+	return 0;
+}
